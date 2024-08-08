220f38029265f275aafaf10b6d2bfb3055dcbbe6
Luis Dolorier <ldolorier@perufarma.com.pe>
2024-08-08 13:08:06 -0500

    repo

--- packages/catbreed_repository/lib/catbreed_repository.dart
+++ packages/catbreed_repository/lib/catbreed_repository.dart
@@ -1,4 +1,5 @@
 /// CatBreeds Repository
 library;
 
+export 'package:app_domain/app_domain.dart';
 export 'src/catbreed_repository.dart';
--- packages/catbreed_repository/lib/src/catbreed_repository.dart
+++ packages/catbreed_repository/lib/src/catbreed_repository.dart
@@ -1,7 +1,79 @@
+import 'package:catbreed_api_client/catbreed_api_client.dart';
+import 'package:catbreed_repository/catbreed_repository.dart';
+
 /// {@template catbreed_repository}
 /// CatBreeds Repository
 /// {@endtemplate}
 class CatbreedRepository {
   /// {@macro catbreed_repository}
-  const CatbreedRepository();
+  const CatbreedRepository({
+    required CatbreedApiClient client,
+  }) : _client = client;
+
+  final CatbreedApiClient _client;
+
+  /// Fetch a list of breeds
+  Future<List<Breed>> fetch({
+    int limit = 10,
+    int page = 0,
+  }) async {
+    try {
+      return await _client.fetch(limit: limit, page: page);
+    } catch (error, stackTrace) {
+      Error.throwWithStackTrace(CatbreedException(error), stackTrace);
+    }
+  }
+
+  /// Search for breeds
+  Future<List<Breed>> search(String query) async {
+    try {
+      return await _client.search(query);
+    } catch (error, stackTrace) {
+      Error.throwWithStackTrace(SearchFailure(error), stackTrace);
+    }
+  }
+
+  /// Fetch a breed by id
+  /// Returns null if the breed is not found
+  Future<Breed?> getBreed(int id) async {
+    try {
+      final breed = await _client.fetchBreed(id);
+      if (breed == null) return null;
+
+      final results = await search(breed.name);
+
+      return results.firstOrNull;
+    } on CatbreedException {
+      rethrow;
+    } catch (error, stackTrace) {
+      Error.throwWithStackTrace(FetchBreedFailure(error), stackTrace);
+    }
+  }
+}
+
+/// {@template catbreed_exception}
+/// Exception thrown by the CatBreeds Repository
+/// {@endtemplate}
+class CatbreedException implements Exception {
+  /// {@macro catbreed_exception}
+  CatbreedException(this.error);
+
+  /// Error thrown by the CatBreeds Repository
+  final Object error;
+}
+
+/// {@template search_failure}
+/// Exception thrown when a search fails
+/// {@endtemplate}
+class SearchFailure extends CatbreedException {
+  /// {@macro search_failure}
+  SearchFailure(super.error);
+}
+
+/// {@template fetch_breed_failure}
+/// Exception thrown when fetching a breed fails
+/// {@endtemplate}
+class FetchBreedFailure extends CatbreedException {
+  /// {@macro fetch_breed_failure}
+  FetchBreedFailure(super.error);
 }
